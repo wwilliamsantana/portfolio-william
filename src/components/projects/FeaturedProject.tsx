28843f75256e54fc8b17e6cd03c1816220369662
William <euwilliamsantana@gmail.com>
2026-08-03 14:31:42 -0300

    fix: Add motions tags

--- src/components/projects/FeaturedProject.tsx
+++ src/components/projects/FeaturedProject.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "motion/react";
 import { ProjectButtons } from "./ProjectButtons";
 import { ProjectImage } from "./ProjectImage";
 import { ProjectStack } from "./ProjectStack";
@@ -18,22 +21,66 @@ interface Props {
 
 export function FeaturedProject({ project }: Props) {
   return (
-    <article>
+    <motion.article
+      initial={{
+        opacity: 0,
+        y: 60,
+        filter: "blur(10px)",
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        filter: "blur(0px)",
+      }}
+      viewport={{
+        once: true,
+        amount: 0.3,
+      }}
+      transition={{
+        duration: 0.7,
+      }}
+    >
       <span className=" rounded-full bg-blue-500/10 px-3 py-1 text-sm font-semibold text-blue-500">
         {project.subtitle}
       </span>
 
-      <h3 className="mt-6 text-5xl font-black ">{project.title}</h3>
+      <motion.h3
+        initial={{
+          opacity: 0,
+          x: -30,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          delay: 0.2,
+        }}
+        className="mt-6 text-5xl font-black "
+      >
+        {project.title}
+      </motion.h3>
 
-      <p className=" mt-6 max-w-3xl text-lg leading-8 text-(--muted)">
+      <motion.p
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        transition={{
+          delay: 0.35,
+        }}
+        className=" mt-6 max-w-3xl text-lg leading-8 text-(--muted)"
+      >
         {project.description}
-      </p>
+      </motion.p>
 
       <ProjectStack stack={project.stack} />
 
       <ProjectButtons demo={project.demo} github={project.github} />
 
       <ProjectImage image={project.image} title={project.title} />
-    </article>
+    </motion.article>
   );
 }
--- src/components/projects/ProjectStack.tsx
+++ src/components/projects/ProjectStack.tsx
@@ -1,18 +1,45 @@
+"use client";
+import { motion } from "motion/react";
+
 interface Props {
   stack: string[];
 }
 
 export function ProjectStack({ stack }: Props) {
   return (
-    <div className="mt-8 flex flex-wrap gap-3">
-      {stack.map((item) => (
-        <span
+    <motion.div
+      initial={{
+        opacity: 0,
+        y: 20,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{
+        delay: 0.7,
+      }}
+      className=" mt-6"
+    >
+      {stack.map((item, index) => (
+        <motion.span
+          initial={{
+            opacity: 0,
+            y: 15,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            delay: 0.45 + index * 0.08,
+          }}
           key={item}
           className=" rounded-full border border-(--border) bg-(--surface) px-4 py-2 text-sm font-medium"
         >
           {item}
-        </span>
+        </motion.span>
       ))}
-    </div>
+    </motion.div>
   );
 }
